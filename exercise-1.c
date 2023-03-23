ee42fc5f1efc44fb8b9dd7aa5e19f41f48774167
EricNunes0 <90226385+EricNunes0@users.noreply.github.com>
2023-03-23 19:19:25 -0300

    Exercise 1 Refeito

--- exercise-1.c
+++ exercise-1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    int vetor[10], i, j, aux;
-    for(i = 0; i <= 9; i++) {
+void main() {
+    int max = 10, i, j, aux, m;
+    int vetor[max];
+    for(i = 0; i <= max - 1; i++) {
         printf("Informe o %dº número: ", i + 1);
         scanf("%d", &aux);
-        for(j = 0; j <= 9; j++) {
+        for(j = 0; j <= max - 1; j++) {
             if(aux == vetor[j]) {
+                printf("(%d é igual a %d)\n", vetor[i], vetor[j]);
                 break;
             }
         }
@@ -16,10 +18,9 @@ int main() {
             i--;
         }
     }
-    int m;
-    for(i = 0; i <= 9; i++) {
+    for(i = 0; i <= max - 1; i++) {
         m = i;
-        for(j = i; j <= 9; j++) {
+        for(j = i; j <= max - 1; j++) {
             if(vetor[i] > vetor[j]) {
                 m = j;
                 int temp = vetor[i];
@@ -29,4 +30,5 @@ int main() {
         }
         printf("(%d) ", vetor[i]);
     }
+    return;
 }
