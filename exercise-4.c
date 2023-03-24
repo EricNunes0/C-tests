2c502d8a781033546b27c7127d56e2355a7e2f8b
EricNunes0 <90226385+EricNunes0@users.noreply.github.com>
2023-03-23 21:43:39 -0300

    Estrutura de Dados em C - Exercício 4 (Incompleto)

--- exercise-4.c
+++ exercise-4.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 
 int main() {
-    int max = 100, filaMax = 6, aux = 1, follow, id;
+    int max = 10, filaMax = 6, aux = 1, follow, id;
     int fila[max], i, j;
     for(i = 0; i <= max; i++) {
         for(j = 0; j <= max; j++) {
             if(j < i) {
-                printf("(%d) ", fila[i]);
+                printf("(%d) ", fila[j]);
             } else {
-                printf("(null) ", fila[i]);
+                printf("(0) ", fila[j]);
             }
         }
-        printf("\nDeseja inserir um novo usuário na fila? (1 - Sim) ");
+        printf("\n1 - Adicionar a fila\n2 - Avançar a fila\nInforme sua próxima ação: ");
         scanf("%d", &follow);
         if(follow == 1) {
-            printf("Informe o ID do usuário: ");
+            int maxCheck = 0, k0;
+            for(int k = 0; k <= max; k++) {
+                if(k < i) {
+                    if(fila[k] != 0) {
+                        maxCheck++;
+                        if(maxCheck >= filaMax) {
+                            printf("\nFila cheia!\n");
+                            fila[i] = id;
+                            i--;
+                            break;
+                        }
+                    }
+                }
+            }
+            printf("\nInforme o ID do usuário: ");
             scanf("%d", &id);
             fila[i] = id;
+        } else {
+            printf("Operação inválida!\n");
+            i--;
         }
     }
 }
