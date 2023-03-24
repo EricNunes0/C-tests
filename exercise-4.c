8642344521afd7e195e47a0041a85ff95aadb552
EricNunes0 <90226385+EricNunes0@users.noreply.github.com>
2023-03-24 19:20:39 -0300

    Estrutura de Dados em C - Exercício 4 (Fila cheia)

--- exercise-4.c
+++ exercise-4.c
@@ -14,23 +14,33 @@ int main() {
         printf("\n1 - Adicionar a fila\n2 - Avançar a fila\nInforme sua próxima ação: ");
         scanf("%d", &follow);
         if(follow == 1) {
-            int maxCheck = 0, k0;
-            for(int k = 0; k <= max; k++) {
+            int maxCheck = 0, k;
+            for(k = 0; k <= max; k++) {
                 if(k < i) {
                     if(fila[k] != 0) {
                         maxCheck++;
                         if(maxCheck >= filaMax) {
                             printf("\nFila cheia!\n");
-                            fila[i] = id;
-                            i--;
                             break;
                         }
                     }
                 }
             }
-            printf("\nInforme o ID do usuário: ");
-            scanf("%d", &id);
-            fila[i] = id;
+            if(k > i) {
+                printf("\nInforme o ID do usuário: ");
+                scanf("%d", &id);
+                fila[i] = id;
+            } else {
+                i--;
+            }
+        } else if(follow == 2) {
+            for(j = 0; j <= max; j++) {
+            if(j < i) {
+                printf("[%d] ", fila[j]);
+            } else {
+                printf("[0] ", fila[j]);
+            }
+        }
         } else {
             printf("Operação inválida!\n");
             i--;
