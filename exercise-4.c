c87237af2c029d326e95170e5838521fe6a90672
EricNunes0 <90226385+EricNunes0@users.noreply.github.com>
2023-04-06 19:45:54 -0300

    Estrutura de Dados em C - Exercício 4

--- exercise-4.c
+++ exercise-4.c
@@ -4,6 +4,7 @@ int main() {
     int max = 10, filaMax = 6, aux = 1, follow, id;
     int fila[max], i, j;
     for(i = 0; i <= max; i++) {
+        printf("◤━━━━━━━━━━━━━━━━━━━━━━━━◥\n");
         for(j = 0; j <= max; j++) {
             if(j < i) {
                 printf("(%d) ", fila[j]);
@@ -11,7 +12,8 @@ int main() {
                 printf("(0) ", fila[j]);
             }
         }
-        printf("\n1 - Adicionar a fila\n2 - Avançar a fila\nInforme sua próxima ação: ");
+        printf("\n◣━━━━━━━━━━━━━━━━━━━━━━━━◢\n");
+        printf("\n💿 1 - Adicionar a fila\n2 - Avançar a fila\nInforme sua próxima ação: ");
         scanf("%d", &follow);
         if(follow == 1) {
             int maxCheck = 0, k;
@@ -27,22 +29,26 @@ int main() {
                 }
             }
             if(k > i) {
-                printf("\nInforme o ID do usuário: ");
+                printf("\n📀 Informe o ID do usuário: ");
                 scanf("%d", &id);
                 fila[i] = id;
             } else {
                 i--;
             }
         } else if(follow == 2) {
-            for(j = 0; j <= max; j++) {
-            if(j < i) {
-                printf("[%d] ", fila[j]);
-            } else {
-                printf("[0] ", fila[j]);
+            fila[0] = 0;
+            for(int k = 0; k <= max; k++) {
+                int temp0 = fila[k];
+                int temp1 = fila[k + 1];
+                fila[k] = temp1;
+                fila[k + 1] = temp0;
             }
-        }
+            i = i - 2;
+            if(i < 0) {
+                i = 0;
+            };
         } else {
-            printf("Operação inválida!\n");
+            printf("❌ Operação inválida!\n");
             i--;
         }
     }
